f3a8017859a95ef4862fcfe693e9f08b4de84137
Mamadou <mamadoua87@yahoo.com>
2024-04-10 12:12:37 -0400

    new code

---
+++ ec2.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "server1" {
+    ami = "ami-0a699202e5027c10d"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [ aws_security_group.sg1.id ]
+    availability_zone = "us-east-1a"
+    subnet_id = aws_subnet.private1.id
+    user_data = file("code.sh")
+    tags= {
+      name = "webserver-1"
+}
+}
+
+resource "aws_instance" "server2" {
+    ami = "ami-0a699202e5027c10d"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [ aws_security_group.sg1.id ]
+    availability_zone = "us-east-1b"
+    subnet_id = aws_subnet.private2.id
+    user_data = file("code.sh")
+    tags= {
+      name = "webserver-2"
+  }
+}
