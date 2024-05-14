4d154e1602ee597da569a5ad9afb862cc0cefa0a
Mamadou <mamadoua87@yahoo.com>
2024-05-14 11:11:29 -0400

    jenkins

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "week10-ma-terraform"
-    key            = "week10-terraform.tfstate"
+    key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "statelog"
